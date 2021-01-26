db2039d63c510cd1ecb454db8c6443f75d9c6096
Aycoul Almotlak <ayoo_66@hotmail.com>
2021-01-26 22:11:10 +0100

    Add files via upload

---
+++ LiteracyComparison/74df4823-6acc-4c66-bba7-49eff3c1ed0f-part10-Part10_13.LiteracyComparison-8840660/src/main/java/LiteracyComparison.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class LiteracyComparison {
+
+    public static void main(String[] args) {
+        try {
+            //row input format: 
+            //Adult literacy rate, population 15+ years, male (%),Angola,2014,82.15105
+            //row desired output format:
+            //Niger (2015), female, 11.01572
+            Files.lines(Paths.get("literacy.csv")).map(x -> x.split(","))
+                    //.sorted((x, y) -> x[5].compareTo(y[5])) this works but is bad
+                    .sorted((x, y) -> { return Double.valueOf(x[5]) < Double.valueOf(y[5]) ? -1 : 1;})
+                    .forEach(row -> System.out.println(row[3] + " (" + row[4] + "), " + row[2].replaceAll("[^a-zA-Z]", "") + ", " + row[5]));
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
