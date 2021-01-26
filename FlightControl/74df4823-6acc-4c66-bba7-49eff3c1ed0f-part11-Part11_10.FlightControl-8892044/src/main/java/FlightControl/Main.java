f02d1b945ff1b3769f9b032ec10d1275f82e912e
Aycoul Almotlak <ayoo_66@hotmail.com>
2021-01-26 22:14:36 +0100

    Add files via upload

---
+++ FlightControl/74df4823-6acc-4c66-bba7-49eff3c1ed0f-part11-Part11_10.FlightControl-8892044/src/main/java/FlightControl/Main.java
@@ -0,0 +1,12 @@
+package FlightControl;
+
+import FlightControl.UI.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        UserInterface ui = new TextUserInterface();
+        ui.run();
+    }
+}
