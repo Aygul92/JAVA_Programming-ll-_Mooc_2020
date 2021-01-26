081915c890d4585c5ffe397ec331cabdc95b619d
Aycoul Almotlak <ayoo_66@hotmail.com>
2021-01-26 22:12:50 +0100

    Add files via upload

---
+++ ReadingFilesPerLine/74df4823-6acc-4c66-bba7-49eff3c1ed0f-part10-Part10_09.ReadingFilesPerLine-8833517/src/main/java/ReadingFilesPerLine.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class ReadingFilesPerLine {
+
+    public static List<String> read(String file) {
+        List<String> fileAsStream = new ArrayList<>();
+        try {
+            fileAsStream = Files.lines(Paths.get(file)).collect(Collectors.toList());
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return fileAsStream;
+    } 
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
